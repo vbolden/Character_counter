c7b3185ed0a34d4f28d81d933100654da7b150a2
Valerie Bolden <valerie.bolden@gmail.com>
2026-04-24 13:18:50 -0400

    added conditionals for error and success class on word count

--- src/components/StatsDisplay/StatsDisplay.tsx
+++ src/components/StatsDisplay/StatsDisplay.tsx
@@ -2,20 +2,34 @@ import type { StatsDisplayProps } from "../../types/index.ts";
 
 const StatsDisplay: React.FC<StatsDisplayProps> = ({ stats, showReadingTime = true }) => {
 
+    const formattedTime = (readingTime: number) => {
+        const totalSeconds = Math.floor(readingTime * 60);
+        const mins = Math.floor(totalSeconds / 60);
+        const secs = totalSeconds % 60;
+
+        return (
+            `${mins}: ${secs.toString().padStart(2, "0")}`
+        );
+    };
+
     return (
         <div className="stats-display">
-            <div>
-                <p>Characters</p>
-                <span>{stats.characterCount}</span>
+            <div className="stat">
+                <p className="label">Characters</p>
+                <span className="value">{stats.characterCount}</span>
             </div>
-            <div>
-                <p>Words</p>
-                <span>{stats.characterCount}</span>
-                <p>Min: 25 | Max: 100</p>
+            <div className="stat">
+                <p className="label">Words</p>
+                <span className={
+                    `value 
+                    ${stats.wordCount > 100 ? "error" : ""} 
+                    ${stats.wordCount >= 25 && stats.wordCount <= 100 ? "success" : ""} 
+                    ${stats.wordCount < 25 ? "error" : ""}`}>{stats.wordCount}</span>
+                <p className="sub">Min: 25 | Max: 100</p>
             </div>
-            <div>
-                <p>Reading Time</p>
-                {showReadingTime && <span>{stats.readingTime}</span>}
+            <div className="stat reading">
+                <p className="label">Reading Time</p>
+                {showReadingTime && <span className="value">{formattedTime(stats.readingTime)}</span>}
             </div>
         </div>
     )

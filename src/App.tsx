64ba4895b00cced70f309015feeaf957a8629609
Valerie Bolden <valerie.bolden@gmail.com>
2026-04-23 21:56:57 -0400

    imported character counter and rendered it

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 import './App.css'
+import CharacterCounter from './components/CharacterCounter/CharacterCounter';
 
 function App() {
 
   return (
-    <>
-      
-    </>
+    <div>
+
+      <CharacterCounter 
+      minWords={0} 
+      maxWords={1000} 
+      targetReadingTime={5} 
+      />
+
+    </div>
   )
 }
 

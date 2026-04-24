47a2d14a3938fccdcebeda2fbd37b63579456e51
Valerie Bolden <valerie.bolden@gmail.com>
2026-04-23 21:14:46 -0400

    added handle change function

--- src/components/TextInput/TextInput.tsx
+++ src/components/TextInput/TextInput.tsx
@@ -1,16 +1,26 @@
-import type { TextInputProps } from "../../types/index.ts";
+import { useState } from "react";
+import type { TextInputProps } from "../../types";
 
 const TextInput: React.FC<TextInputProps> = ({
     onTextChange,
     placeholder = "Type your message here...",
     initialValue = ''
 }) => {
+
+    const [text, setText] = useState(initialValue)
+
+    const handleChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+        const newText = event.target.value;
+        setText(newText);
+        onTextChange(newText) // SEND DATA TO PARENT
+    };
+
     return (
         <div className="textbox">
             <textarea 
             placeholder={placeholder} 
-            defaultValue={initialValue} 
-            onChange={(e) => onTextChange(e.target.value)} 
+            value={text} 
+            onChange={handleChange} 
             rows={6} ></textarea>
         </div>
     )
